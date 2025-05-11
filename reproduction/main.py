902279b8d808bf537907c6d06b6272542be6e562
Brian <brian030128@gmail.com>
2025-05-12 03:32:01 +0800

    update

--- reproduction/main.py
+++ reproduction/main.py
@@ -92,9 +92,7 @@ parameters = [
 test_model(ModelType.PHI35, [], [
     (9 , 1000)
 ])
-# test_model(ModelType.LLAMA3, 
-#            [    (3, 1000),
-#                 (9, 1000), (15 , 1000)], parameters)
+test_model(ModelType.LLAMA3, parameters, parameters)
 # test_model(ModelType.MISTRAL, 
 #            [(3, 1000), (6, 1000)],
 #            [(1, 1000),(3, 1000), (6, 1000)])
