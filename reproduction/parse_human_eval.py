d9d275eaab965136b2ec8a8a69fed4272e87af89
Brian <brian030128@gmail.com>
2025-05-11 13:07:30 +0800

    update

--- reproduction/parse_human_eval.py
+++ reproduction/parse_human_eval.py
@@ -76,22 +76,22 @@ for model in source.keys():
 for model in source.keys():
     for decode_type in source[model].keys():
         for file_name in source[model][decode_type]:
-                in_1_path = f"out/{model}/{decode_type}/GSM8K/{file_name}"
-                in_2_path = f'tmp_out/{model}/{decode_type}/HUMAN_EVAL/{file_name}'
-                o_path = f"final_out/{model}/{decode_type}/GSM8K/{file_name}"
-                os.makedirs(f'final_out/{model}/{decode_type}/GSM8K/', exist_ok=True)
-                entries = []
-                scores = []
-                with open(in_2_path, 'r') as f:
-                    for idx, line in enumerate(f):
-                        obj = json.loads(line)
-                        scores.append(1 if obj["passed"] else 0)
-                with open(in_1_path, 'r') as f:
-                    for idx, line in enumerate(f):
-                        obj = json.loads(line)
-                        obj['score'] = scores[idx]
-                        entries.append(obj)
-                
-                with open(o_path, 'w') as f:
-                    for entry in entries:
-                        f.write(json.dumps(entry) + '\n')
+            in_1_path = f"out/{model}/{decode_type}/HUMAN_EVAL/{file_name}"
+            in_2_path = f'tmp_out/{model}/{decode_type}/HUMAN_EVAL/{file_name}'
+            o_path = f"final_out/{model}/{decode_type}/HUMAN_EVAL/{file_name}"
+            os.makedirs(f'final_out/{model}/{decode_type}/HUMAN_EVAL/', exist_ok=True)
+            entries = []
+            scores = []
+            with open(in_2_path, 'r') as f:
+                for idx, line in enumerate(f):
+                    obj = json.loads(line)
+                    scores.append(1 if obj["passed"] else 0)
+            with open(in_1_path, 'r') as f:
+                for idx, line in enumerate(f):
+                    obj = json.loads(line)
+                    obj['score'] = scores[idx]
+                    entries.append(obj)
+            
+            with open(o_path, 'w') as f:
+                for entry in entries:
+                    f.write(json.dumps(entry) + '\n')
