f6bb1632e3ecbd7536f56313330acb8c7fe40118
Brian <brian030128@gmail.com>
2025-05-17 20:08:53 +0800

    update

--- reproduction/tree_decoding.py
+++ reproduction/tree_decoding.py
@@ -217,7 +217,7 @@ def generate_next_tokens(model, input_ids, beam_width = 3, max_new_tokens=300,eo
     # (the clone itself is always small)
     next_token_logits = outputs.logits.clone()[:, -1, :].float()
     next_token_logits = next_token_logits.to(input_ids.device)
-    del outputs.logits
+    outputs.logits = None
     torch.cuda.empty_cache()
     gpu_gc.collect()
     past_key_values = outputs.past_key_values
