f65fcbd20823df347c0d0ece937f6c13267c1194
Brian <brian.chan@linecorp.com>
2025-05-20 13:06:24 +0800

    update

--- reproduction/gc_overhead_test.py
+++ reproduction/gc_overhead_test.py
@@ -404,7 +404,7 @@ def run ():
 
     prompt = "Hi my name is Brian." * 100
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(model.device)
-    output = generate_next_tokens(model, input_ids, beam_width=3, max_new_tokens=10, eos_token_id=[model.config.eos_token_id])
+    output = generate_next_tokens(model, input_ids, beam_width=30, max_new_tokens=10, eos_token_id=[model.config.eos_token_id])
     print("input length: ", input_ids.shape[1])
     torch.cuda.synchronize()
     start = time.time()
