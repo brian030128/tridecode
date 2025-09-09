09a70620004edcd235770dddbe1cf613bc6f95a4
Brian <brian.chan@linecorp.com>
2025-09-09 12:08:28 +0800

    update

--- reproduction/main.py
+++ reproduction/main.py
@@ -112,11 +112,11 @@ def test_model(model_type:ModelType, tree_params, origin_params):
 
 # beams / max_tokens
 parameters = [
-    (9, 2000)
+    (6, 2000)
 ]
 
 trie_paramters = [
-    (9, 2000)
+    (6, 2000)
 ]
 
 #test_model(ModelType.PHI35, trie_paramters, parameters)
